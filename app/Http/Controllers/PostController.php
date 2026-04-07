cc901f5bd0ec92531be90484d7e1045c23095324
naoki148710 <naoki2906@outlook.jp>
2026-04-07 00:57:54 +0000

    blogdetails

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,14 +5,26 @@ namespace App\Http\Controllers;
 //use宣言は外部にあるクラスをPostController内にインポートできる。
 //この場合、App\Models内のPostクラスをインポートしている。
 use App\Models\Post;
+
 use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
     public function index(Post $post) //インポートしたPostをインスタンス化して$postとして使用。
     {
 
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(3)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
         //getPaginateByLimit()はPost.phpで定義したメソッドです。
+    }
+    /*
+特定IDのpostを表示する
+@params Object Post // 引数の$postはid=1のPostインスタンス
+@return Reposnse post view
+*/
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
 }
