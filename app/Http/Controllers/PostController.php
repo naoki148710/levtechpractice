0385c362f29a5b3b0a406e856a7ebbeeceb9e9aa
naoki148710 <naoki2906@outlook.jp>
2026-04-13 14:54:42 +0000

    blogup screen creation and Process execution

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,22 @@ class PostController extends Controller
     public function index(Post $post) //インポートしたPostをインスタンス化して$postとして使用。
     {
 
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(3)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
         //getPaginateByLimit()はPost.phpで定義したメソッドです。
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
 }
